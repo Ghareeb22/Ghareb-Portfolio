4101f9a67a0d80be8e4d3da1c4fbd124f11bf749
MohamedGhareb <122824085+Ghareeb22@users.noreply.github.com>
2023-09-16 16:48:25 +0300

    Update footer.js

--- src/Componentes/footer.js
+++ src/Componentes/footer.js
@@ -1,46 +1,81 @@
 import "./footer.css";
 
-import React from 'react'
+import React from "react";
 
-import { FaFacebook, FaHome, FaLinkedin, FaMailBulk, FaPhone, FaTwitter } from "react-icons/fa"
+import {
+  FaFacebook,
+  FaHome,
+  FaLinkedin,
+  FaMailBulk,
+  FaPhone,
+  FaTwitter,
+} from "react-icons/fa";
 const footer = () => {
   return (
     <div className="footer">
-        <div className="footer-container">
-            <div className="left">
-                <div className="location">
-                    <FaHome size={20} style={{color: "#FFF", marginRight:"2rem" }}/>
-                
-                <div>
-                    <p>5th Settlement <br/> The Garden Compound</p>
-                    <p>Cairo, Egypt</p>
-                    </div>
-                    </div>
-                    <div className="phone">
-                    <h4><FaPhone size={20} style={{color: "#FFF", marginRight:"2rem" }}/>
-                    01145776860</h4>
-                    </div>  
-                    <div className="email">
-                    <h4><FaMailBulk size={20} style={{color: "#FFF", marginRight:"2rem" }}/>
-                    moghareb22@gmail.com</h4>
-                   
-                    </div>
-            </div>
-            <div className="right">
-            <h4>About me</h4>
-            <p>My name is Mohamed and I got graduated in August 2022. <br/>
-                I love coding and I am willing to be perfect at my field always,<br/>
-                also I am a react developer.
-            </p>
-            <div className="social">
-            <FaFacebook size={20} style={{color: "#FFF", marginRight:"2rem" }}/>
-            <FaLinkedin size={20} style={{color: "#FFF", marginRight:"2rem" }}/>
-            <FaTwitter size={20} style={{color: "#FFF", marginRight:"2rem" }}/>
-            </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color: "#FFF", marginRight: "2rem" }} />
 
-export default footer
+            <div>
+              <p>
+                5th Settlement <br /> The Garden Compound
+              </p>
+              <p>Cairo, Egypt</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#FFF", marginRight: "2rem" }}
+              />
+              01145776860
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#FFF", marginRight: "2rem" }}
+              />
+              moghareb22@gmail.com
+            </h4>
+          </div>
+        </div>
+        <div className="right">
+          <h4>About me</h4>
+          <p>
+            My name is Mohamed and I graduated in August 2022. <br />
+            I love coding and I am willing to be perfect at my field always,
+            <br />
+            also I am a react developer.
+          </p>
+          <div className="social">
+            <a href="https://www.facebook.com/muhammed.ash.184/">
+              <FaFacebook
+                size={20}
+                style={{ color: "#FFF", marginRight: "2rem" }}
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/mohamed-ghareb-447112253/">
+              <FaLinkedin
+                size={20}
+                style={{ color: "#FFF", marginRight: "2rem" }}
+              />
+            </a>
+            <a href="https://twitter.com/?lang=en">
+              <FaTwitter
+                size={20}
+                style={{ color: "#FFF", marginRight: "2rem" }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default footer;
