4ac5418ea6ae89daa76552f2d7c9a4676aa92130
MohamedGhareb <122824085+Ghareeb22@users.noreply.github.com>
2023-09-16 16:46:22 +0300

    Update AboutContent.js

--- src/Componentes/AboutContent.js
+++ src/Componentes/AboutContent.js
@@ -1,6 +1,6 @@
 import "./AboutContent.css";
 
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 import React1 from "../assests/react1.jpg";
 import React2 from "../assests/react2.webp";
@@ -9,24 +9,26 @@ export const AboutContent = () => {
     <div className="about">
       <div className="left">
         <h1>Who Am I ?</h1>
-        <p>I'm A Front-End React Developer.<br/> Creating Responsive Web Pages by using React.</p>
+        <p>
+          I'm A Front-End React Developer.
+          <br /> Creating Responsive Web Pages by using React.
+        </p>
         <Link to={"/Contact"}>
-        <button className="btn">Contact</button>
-      </Link>
+          <button className="btn">Contact</button>
+        </Link>
       </div>
       <div className="right">
         <div className="img-container">
           <div className="img-stack top">
-            <img src={React1} className="img" alt="true"/>
+            <img src={React1} className="img" alt="true" />
           </div>
           <div className="img-stack bottom">
-            <img src={React2} className="img" alt="true"/>
+            <img src={React2} className="img" alt="true" />
           </div>
         </div>
       </div>
-   
     </div>
-  )
-}
+  );
+};
 
-export default  AboutContent;
+export default AboutContent;
