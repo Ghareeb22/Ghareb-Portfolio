f7c542ea7ad1a37e0e855c5dfb81ea5db757a539
MohamedGhareb <122824085+Ghareeb22@users.noreply.github.com>
2023-09-16 16:46:43 +0300

    Update HeroImage.js

--- src/Componentes/HeroImage.js
+++ src/Componentes/HeroImage.js
@@ -1,27 +1,29 @@
 import "./ImgStyles.css";
 
-import React from 'react'
+import React from "react";
 
-import  introImg from "../assests/intro-bg.jpg"
+import introImg from "../assests/intro-bg.jpg";
 import { Link } from "react-router-dom";
 const HeroImage = () => {
   return (
-    <div className="hero">HeroImage
-    <div className="mask">
-     <img className="intro-img" src={introImg} alt="IntroImg"/>
-    </div>
-    <div className="content">
-      <p>HI I AM A FRONT-END DEVELOPER</p>
-      <h1>React Developer</h1>
-      <div><Link to="/project" className="btn">Projects
-      </Link>
-      <Link to="/contact" className="btn btn-light">Contact
-      </Link>
+    <div className="hero">
+      <div className="mask">
+        <img className="intro-img" src={introImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p>HI I AM A FRONT-END DEVELOPER</p>
+        <h1>React Developer</h1>
+        <div>
+          <Link to="/project" className="btn">
+            Projects
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Contact
+          </Link>
+        </div>
       </div>
     </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default HeroImage
+export default HeroImage;
