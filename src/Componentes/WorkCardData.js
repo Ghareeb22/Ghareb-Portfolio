5c4ed8d9b9a91002dcaa8bfb6927924109a0bad3
MohamedGhareb <122824085+Ghareeb22@users.noreply.github.com>
2023-09-16 16:44:04 +0300

    Update WorkCardData.js

--- src/Componentes/WorkCardData.js
+++ src/Componentes/WorkCardData.js
@@ -1,24 +1,24 @@
-import pro1 from "../assests/project1.png"
-import pro2 from "../assests/project2.png"
-import pro3 from "../assests/project3.png"
+import pro1 from "../assests/download3.png";
+import pro2 from "../assests/download.png";
+import pro3 from "../assests/download2.png";
 const ProjectCardData = [
-    {
-        imgsrc:pro1,
-        title: "Ecommerce Website Design",
-        text: "Thriving Facebook community and top-ranked podcast, and they can order whatever food they desire as long as they can see it already at the food menu partiton on our website.",
-        view :"https://www.youtube.com/watch?v=P8YuWEkTeuE"
-    },
-    {
-        imgsrc:pro2,
-        title: "Computers Parts Shop ",
-        text: "Thriving Facebook community and top-ranked podcast, and they can order whatever food they desire as long as they can see it already at the food menu partiton on our website.",
-        view :"https://www.youtube.com/watch?v=P8YuWEkTeuE"
-    },
-    {
-        imgsrc:pro3,
-        title: "Clothes Design Website",
-        text: "Thriving Facebook community and top-ranked podcast, and they can order whatever food they desire as long as they can see it already at the food menu partiton on our website.",
-        view :"https://www.youtube.com/watch?v=P8YuWEkTeuE"
-    }
-]
+  {
+    imgsrc: pro1,
+    title: "Real Estate Website",
+    text: "Welcome to our Real Estate Website, your one-stop destination for finding your dream home. Browse through our extensive listings of properties for sale or rent, ranging from cozy apartments to spacious houses.",
+    view: "https://real-estate-react-app-6dt.pages.dev/",
+  },
+  {
+    imgsrc: pro2,
+    title: "Pizzeria OldTown",
+    text: "Welcome to Old Town Pizzeria, your go-to destination for mouthwatering pizza. With our delicious homemade pizza, top-quality ingredients, we guarantee a slice of perfection in every bite.",
+    view: "https://pizzeria-website.pages.dev/",
+  },
+  {
+    imgsrc: pro3,
+    title: "Travelling Agency",
+    text: "Welcome to our Travel Agency Website, your one-stop destination for finding your dream trip. Browse through our extensive listings of offers and deals, ranging from a fancy jorunies and trips.",
+    view: "https://travel-website-2.pages.dev/",
+  },
+];
 export default ProjectCardData;
